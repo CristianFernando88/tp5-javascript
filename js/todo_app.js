a227f83eacbfb3dbea648f14e4b1bb6ba9054666
CristianFernando <gordyfernando@gmail.com>
2026-04-25 23:34:11 -0300

    cambio estructura todo-app

---
+++ js/todo_app.js
@@ -0,0 +1,54 @@
+let tareas = [
+    {id:1, titulo:"Comprar leche", completada:false},
+    {id:2, titulo:"Lavar el auto", completada:true},
+    {id:3, titulo:"Estudiar para el examen", completada:false}
+];
+const containerTareas = document.getElementById("container-tareas");
+const crearTareaHTML = (tarea)=>{
+    return `
+        <div class="tarea ">
+            <div>
+                <h3 class="${tarea.completada ? 'tarea-completada' : ''}">${tarea.titulo}</h3>
+                <input type="checkbox" onchange="toggleTarea(${tarea.id})" ${tarea.completada ? 'checked' : ''}>
+            </div>
+            <button class="button" onclick="eliminarTarea(${tarea.id})">
+                <i class="fa-solid fa-trash-can"></i>
+            </button>
+        </div>
+    `;
+}
+const cargarTareas = ()=>{
+    containerTareas.innerHTML = "";
+    tareas.map(tarea=>{
+        containerTareas.innerHTML += crearTareaHTML(tarea);
+    }).join("");
+}
+
+const toggleTarea = (id)=>{
+    tareas = tareas.map(tarea=>{
+        if(tarea.id == id){
+            return {...tarea, completada: !tarea.completada};
+        }
+        return tarea;
+    });
+    cargarTareas();
+}
+
+const eliminarTarea = (id)=>{
+    tareas = tareas.filter(tarea=>tarea.id != id);
+    cargarTareas();
+}
+const agregarTarea = (e)=>{
+    e.preventDefault();
+    const inputTitulo = document.getElementById("tituloTarea");
+    if(inputTitulo.value.trim() === "") return;
+    const nuevaTarea = {
+        id: Date.now(),
+        titulo: inputTitulo.value,
+        completada: false
+    }
+    tareas.push(nuevaTarea);
+    inputTitulo.value = "";
+    cargarTareas();
+}
+cargarTareas();
